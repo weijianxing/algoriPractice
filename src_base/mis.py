c36829379f4f7a3455c113b834d39055513be936
魏建星 <weijx@me.com>
2021-03-10 21:17:08 +0800

    add practice code

--- src_base/mis.py
+++ src_base/mis.py
@@ -158,11 +158,27 @@ class Solution:
             ret += 1
         return len(s)
 
+    def singleNumber(self, nums: list) -> int:
+
+#         交换律：a ^ b ^ c <=> a ^ c ^ b
+#
+# 任何数于0异或为任何数 0 ^ n => n
+#
+# 相同的数异或为0: n ^ n => 0
+#
+# var a = [2,3,2,4,4]
+#
+# 2 ^ 3 ^ 2 ^ 4 ^ 4等价于 2 ^ 2 ^ 4 ^ 4 ^ 3 => 0 ^ 0 ^3 => 3
+        a = 0
+        for num in nums:
+            a = a ^ num
+        return a
+
 if __name__ == '__main__':
 
     # data = [1,1]
     s = Solution()
-    print(s.lengthOfLastWord("a "))
+    print(s.singleNumber([1,3,1,-1,3]))
 
     # strs =[]
     # print(s.longestCommonPrefix(strs))
