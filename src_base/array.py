39ac17f2740223b75f2ddacfb05c97a516f0c7bb
魏建星 <weijx@me.com>
2021-03-25 22:45:50 +0800

    add practice code

--- src_base/array.py
+++ src_base/array.py
@@ -44,11 +44,26 @@ class Solution():
         for n1 in nums1:
             for n2 in nums2:
                 pass
+    def maxProfit(self, prices: list) -> int:
+        size = len(prices)
+        buyprice = prices[0]
+        maxprof = 0
+        for i in range(1, size):
+            buyprice = min(prices[i],buyprice)
+            maxprof = max(maxprof, prices[i] - buyprice)
+        return maxprof
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    nums = [4,3,2,7,8,2,3,1]
+    nums =  [7,1,5,3,6,4]
     # nums.append([4,3,2,7,8,2,3,1])
     s = Solution()
-    print(s.findDisappearedNumbers(nums))
+    print(s.maxProfit(nums))
 
 
 
